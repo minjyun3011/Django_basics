a7ee1cd9e3b1d77828b0b84273cc3a3b53f0c497
minjyun3011 <grand3canyon10@gmail.com>
2024-02-14 20:38:12 +0900

    コメント機能まで追加完了

--- microblog/microblog/urls.py
+++ microblog/microblog/urls.py
@@ -2,10 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from blog.views import frontpage
+from blog.views import frontpage, post_detail
 #frontpage関数がどこにあるのかを教える役割
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", frontpage)
+    path("", frontpage),
+    path("<slug:slug>/", post_detail, name="post_detail")
 ]
